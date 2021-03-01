990e825467ff7fc003ea212f673cad8b1d3597a4
leo0807 <zhangjunxu3@gmail.com>
2021-03-01 11:49:22 +0800

    Leetcode 1557

--- Leetcode/1557.ts
+++ Leetcode/1557.ts
@@ -1,9 +1,11 @@
 function findSmallestSetOfVertices(n: number, edges: number[][]): number[] {
     let map: number[] = new Array(n);
     let res: number[] = [];
+    // 找到能到达的边的值
     for(let [_, val] of edges){
         map[val] = 1;
     }
+     //筛选出不能到达的边即为结果
     for(let i = 0; i < n; i++){
         if(!map[i]) res.push(i);
     }

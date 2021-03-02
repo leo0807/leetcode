dcc6557333bee8c8ff64d3e58f411bbd3e0ee748
leo0807 <zhangjunxu3@gmail.com>
2021-03-03 00:14:54 +0800

    Leetcode 145

--- Leetcode/145.ts
+++ Leetcode/145.ts
@@ -30,3 +30,29 @@ function postorderTraversal(root: TreeNode | null): number[] {
     }
     return res;
 };
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function postorderTraversal2(root: TreeNode | null): number[] {
+    let res: number[] = [];
+    recursive(root, res);
+    return res;
+
+};
+function recursive(root: TreeNode | null, res: number[]): void {
+    if(!root) return;
+    recursive(root.left, res);
+    recursive(root.right, res);
+    res.push(root.val);
+};

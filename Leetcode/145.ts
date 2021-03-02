5782febcc178175711cd7e8a00a31b2eba6db4e9
leo0807 <zhangjunxu3@gmail.com>
2021-03-03 00:11:03 +0800

    Leetcode 145

---
+++ Leetcode/145.ts
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function postorderTraversal(root: TreeNode | null): number[] {
+    let res: number[] = [];
+    if(!root) return res;
+    let stack: TreeNode[] = [root];;
+    while(stack.length){
+        let node: TreeNode | null = stack[stack.length - 1];
+        if(node.left){
+            stack.push(node.left);
+            node.left = null;
+        }else if(node.right){
+            stack.push(node.right);
+            node.right = null;
+        }else{
+            res.push(stack.pop().val);
+        }
+    }
+    return res;
+};

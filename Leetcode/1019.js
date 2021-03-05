bb8da0662d32c4dd7832ecbb9b49186e53172be9
leo0807 <zhangjunxu3@gmail.com>
2021-03-05 23:39:15 +0800

    Leetcode

--- Leetcode/1019.js
+++ Leetcode/1019.js
@@ -1,14 +1,9 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} head
- * @return {number[]}
- */
+
+function ListNode(val, next) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+}
+
 var nextLargerNodes = function (head) {
     let stack = [],
         res = [];

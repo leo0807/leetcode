8e8586d16b5f77086b0f90b12bbc76c3719eedc2
leo0807 <zhangjunxu3@gmail.com>
2021-03-26 20:51:22 +0800

    clouser fibonacci

---
+++ Js常见编程面试题目/闭包斐波那契数列.js
@@ -0,0 +1,11 @@
+let fibonacci = function () {
+    let temp = [0, 1];
+    return function (n) {
+        let result = temp[n];
+        if (typeof result != 'number') {
+            result = fibonacci(n - 1) + fibonacci(n - 2);
+            temp[n] = result; // 将每次 fibonacci(n) 的值都缓存下来
+        }
+        return result;
+    }
+}();

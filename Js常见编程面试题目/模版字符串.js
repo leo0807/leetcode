059a4f7f000ebe76e86054366a425771cb029206
leo0807 <zhangjunxu3@gmail.com>
2021-04-05 23:16:05 +0800

    template String

---
+++ Js常见编程面试题目/模版字符串.js
@@ -0,0 +1,24 @@
+function templateStr(template, data) {
+    for (let i in data) {
+        let index = template.indexOf(i);
+        if (index > -1 && template.slice(index - 2, index) === '${'
+            && template[index + 1 + i.length] === '}') {
+            template.replace()
+        }
+    }
+}
+
+let temp = '我是{{name}}，年龄{{age}}，性别{{sex}}';
+let data1 = {
+    name: '姓名',
+    age: 18
+}
+function template(pattern, data) {
+    return pattern.replace(/\{\{(\w+)\}\}/g, function (match, key, index) {
+        console.log("match", match, "key", key, "index", index);
+        return data[key] || "";
+    })
+}
+
+
+console.log(template(temp, data1));

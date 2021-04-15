8097b75322427e1671a60a7f5ee760708f69ade5
leo0807 <zhangjunxu3@gmail.com>
2021-04-15 23:53:46 +0800

    URL valid

---
+++ Js常见编程面试题目/判断合法URL.js
@@ -0,0 +1,8 @@
+const isUrl = urlStr => {
+    try {
+        const { href, origin, host, hostname, pathname } = new URL(urlStr)
+        return href && origin && host && hostname && pathname && true
+    } catch (e) {
+        return false
+    }
+}

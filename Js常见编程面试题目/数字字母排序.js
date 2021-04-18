a160c86b5c534aa6063a9e03490544546898fc1f
leo0807 <zhangjunxu3@gmail.com>
2021-04-18 17:26:12 +0800

    update

--- Js常见编程面试题目/数字字母排序.js
+++ Js常见编程面试题目/数字字母排序.js
@@ -1,5 +1,5 @@
 let str = "fed123ab456c";
-
+// 数字在前 字母在后
 function mixedSorted(str) {
     return str.split('').sort((a, b) => {
         if (a.charCodeAt() > b.charCodeAt()) {
@@ -11,3 +11,14 @@ function mixedSorted(str) {
 }
 
 console.log(mixedSorted(str));
+// 字母在前 数字在后
+let str = "a4sa4da5da6";
+function sortArr(str) {
+    return str.split('').sort((a, b) => {
+        a = isNaN(a) ? a.charCodeAt() : a.charCodeAt() + 75;
+        b = isNaN(b) ? b.charCodeAt() : b.charCodeAt() + 75;
+        return a - b;
+    }).join("");
+}
+
+console.log(sortArr(str));

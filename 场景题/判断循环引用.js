e612092c035c6f737d39baafeeeadca20e6b27d1
leo0807 <zhangjunxu3@gmail.com>
2021-04-21 17:26:30 +0800

    isCycle

--- 场景题/判断循环引用.js
+++ 场景题/判断循环引用.js
@@ -1,16 +1,47 @@
 var a = {
-    b: null,
+    b: { d: null },
     c: null
 };
-a.b = a;
-a.c = a.c;
+a.b.d = a;
+// a.c = a.c;
 
-function circleReference(obj) {
-    for (let i in obj) {
-        if (typeof obj[i] === 'object' && obj[i] === obj) {
-            return true;
-        }
+function isCycle(obj) {
+    try {
+        JSON.stringify(obj);
+        return false;
+    } catch (e) {
+        return true;
     }
-    return false;
 }
-console.log(circleReference(a));
+
+function isCycle2(obj) {
+    const seenObjs = [];
+    const detect = (obj) => {
+        if (typeof obj === 'object') {
+            if (seenObjs.indexOf(obj) > -1) {
+                return true;
+            }
+        }
+        seenObjs.push(obj);
+        for (let i in obj) {
+            if (obj.hasOwnProperty(i) && detect(obj[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    return detect(obj);
+}
+
+
+// function circleReference(obj) {
+//     for (let i in obj) {
+//         if (typeof obj[i] === 'object' && obj[i] === obj) {
+//             return true;
+//         }
+//     }
+//     return false;
+// }
+// console.log(circleReference(a));
+console.log(isCycle2(a));

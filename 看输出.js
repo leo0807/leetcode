2cb1cb4fc81959e38cfd6799801904a859dd0c53
leo0807 <zhangjunxu3@gmail.com>
2021-04-23 12:35:36 +0800

    update

--- 看输出.js
+++ 看输出.js
@@ -109,3 +109,53 @@ a[c] = 'c';
 
 // 输出 c
 console.log(a[b]);
+
+
+new Promise(function (resolve) {
+    for (var i = 0; i < 10; i++) {
+        resolve(i);
+    }
+}).then(function (i) {
+    console.log(i);
+});
+new Promise(function (resolve) {
+    for (var i = 0; i < 10; i++) {
+        function a() {
+            resolve(i);
+        }
+    }
+    a();
+}).then(function (i) {
+    console.log(i);
+});
+// 上面打印 0
+// 下面打印 10（因为使用 var 定义是函数作用域，不会为每一个 i 保存一块内存）
+
+// Q1
+var a = 1;
+function print() {
+    console.log(this.a)
+}
+print() // 1
+// Q2
+const obj = {
+    a: 2,
+    print: function () { console.log(this.a) }
+}
+obj.print(); // 2
+// Q3
+function Person() {
+    this.a = 8
+    this.print = function () { console.log(this.a) }
+    return { a: 9 }
+}
+const p = new Person()
+console.log(p.a) // 9
+console.log(p.print()) // 报错，返回的对象中没有 print 方法
+// Q4
+'use strict';
+var a = 1;
+function print() {
+    console.log(this.a)
+}
+print() // 报错 不能给 undefined 添加属性

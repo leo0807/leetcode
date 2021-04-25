fda63eba586cb86c04effca816880ff08384d565
leo0807 <zhangjunxu3@gmail.com>
2021-04-25 09:05:18 +0800

    dd new solution

--- Leetcode/3.js
+++ Leetcode/3.js
@@ -73,3 +73,4 @@ var lengthOfLongestSubstring4 = function (s) {
     return res;
 };
 console.log(lengthOfLongestSubstring3(" "));
+// 找到以arr[end]结尾的最长子串，然后max保持最大长度，当某个数在之前出现过，这个时候就把子串的起点start往后推一个，但是有一种情况，比如1，2，3，4，3，5，1。到第二个3时，以后的子串起点start为4，到第二个1时，如果不取最大的start，按start = map.get(arr[end]) + 1 算出起点start为2，显然以起点start = 2，结尾end = 1的子串234351有重复的，因此start要取最大的

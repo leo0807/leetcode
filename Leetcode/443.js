29860a03ad3b72d9a7608f6faea9945bb68aa488
leo0807 <zhangjunxu3@gmail.com>
2021-04-29 14:36:34 +0800

    443

--- Leetcode/443.js
+++ Leetcode/443.js
@@ -22,3 +22,27 @@ var compress = function (chars) {
     return res;
 };
 
+/**
+ * @param {character[]} chars
+ * @return {number}
+ */
+var compress2 = function (chars) {
+    let left = 0, right = 0, len = chars.length, tmp = [];
+    while (right < len) {
+        right++;
+        while (right < len && chars[left] === chars[right]) {
+            right++;
+        }
+        tmp.push(chars[left]);
+        if (right - left > 1) {
+            tmp.push((right - left).toString());
+        }
+        left = right;
+    }
+    tmp = tmp.join("");
+    tmp = tmp.split('');
+    for (let i = 0, len1 = tmp.length; i < len1; i++) {
+        chars[i] = tmp[i];
+    }
+    return tmp.join("").length;
+};

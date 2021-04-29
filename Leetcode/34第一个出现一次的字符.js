bf7a3ff0678d8ea33027601e8887b9461ea1fde2
leo0807 <zhangjunxu3@gmail.com>
2021-04-29 14:36:25 +0800

    update new approach

--- Leetcode/34第一个出现一次的字符.js
+++ Leetcode/34第一个出现一次的字符.js
@@ -7,3 +7,30 @@ function FirstNotRepeatingChar(str) {
     }
     return -1;
 }
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function (nums, target) {
+    let left = 0, right = nums.length - 1;
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (nums[mid] === target) {
+            let resLeft = mid, resRight = mid;
+            while (nums[resLeft] === target) {
+                resLeft--;
+            }
+            while (nums[resRight] === target) {
+                resRight++;
+            }
+            return [++resLeft, --resRight];
+        } else if (nums[mid] > target) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return [-1, -1];
+};

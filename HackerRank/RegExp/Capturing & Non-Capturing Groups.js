59ef6af8203a6610ce988b8b9b1ae7da51e327a3
leo0807 <zhangjunxu3@gmail.com>
2021-05-04 12:02:37 +0800

    group

---
+++ HackerRank/RegExp/Capturing & Non-Capturing Groups.js
@@ -0,0 +1,7 @@
+var Regex_Pattern = /(ok){3,}/; //Do not delete '/'. Replace __________ with your regex.
+
+function processData(Test_String) {
+    //Enter your code here
+
+    console.log(!!Test_String.match(Regex_Pattern));
+}

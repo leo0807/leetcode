21ca12eb5325502efdf4ef6f55ad4472ea976278
leo0807 <zhangjunxu3@gmail.com>
2021-08-03 17:33:19 +0800

    1880

--- Leetcode/1880.js
+++ Leetcode/1880.js
@@ -7,8 +7,6 @@
 var isSumEqual = function (firstWord, secondWord, targetWord) {
     return helper(firstWord) + helper(secondWord) === helper(targetWord);
 };
-
-
 function helper(s) {
     return Number(s.split("").map(curr => (curr.charCodeAt(0) - 97).toString()).reduce((prev, curr) => prev + curr))
 }

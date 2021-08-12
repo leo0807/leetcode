d4960371d6e5349db6d341c01b8e716efa9a0e35
leo0807 <zhangjunxu3@gmail.com>
2021-08-12 18:39:27 +0800

    343

--- Leetcode/343.js
+++ Leetcode/343.js
@@ -14,3 +14,13 @@ var integerBreak = function (n) {
     product *= n;
     return product;
 };
+
+const integerBreak2 = function (n) {
+    const dp = new Array(n + 1).fill(1);
+    for (let i = 2; i <= n; i++) {
+        for (let j = 1; j < i; j++) {
+            dp[i] = Math.max(dp[i](Math.max(j, dp[j])) * (Math.max(i - j, dp[i - j])));
+        }
+    }
+    return dp[n];
+}

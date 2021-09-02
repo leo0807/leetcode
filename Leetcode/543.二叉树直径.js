377bbbc3e635dc668dc82017083369e0ad781e3a
leo0807 <zhangjunxu3@gmail.com>
2021-09-02 09:09:29 +0800

    update

--- Leetcode/543.二叉树直径.js
+++ Leetcode/543.二叉树直径.js
@@ -13,3 +13,28 @@ var diameterOfBinaryTree = function (root) {
     return max;
 };
 //直径就是树中任意两个最短点的最大距离
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var diameterOfBinaryTree1 = function (root) {
+    let max = 0;
+    getMaxDia(root);
+    return max;
+    function getMaxDia(root) {
+        if (root.left === null && root.right === null) return 0;
+        let left = root.left === null ? 0 : 1 + getMaxDia(root.left);
+        let right = root.right === null ? 0 : 1 + getMaxDia(root.right);
+        max = Math.max(left + right, max);
+        return Math.max(left, right);
+    }
+};

aea56972a55c78c9087c1e5f1ff507f390f1f19d
leo0807 <zhangjunxu3@gmail.com>
2021-09-02 23:54:06 +0800

    116

---
+++ Leetcode/116.js
@@ -0,0 +1,24 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+var connect = function (root) {
+    if (!root || !root.left) return root;
+    root.left.next = root.right;
+    if (root.next) {
+        root.right.next = root.next.left;
+    }
+    root.left = connect(root.left);
+    root.right = connect(root.right);
+    return root;
+};

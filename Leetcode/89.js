37461b941293d152f7855d931b238d82c14afb8a
leo0807 <zhangjunxu3@gmail.com>
2021-09-02 10:18:11 +0800

    89

--- Leetcode/89.js
+++ Leetcode/89.js
@@ -8,3 +8,17 @@ function factorial(n) {
     }
     return res;
 }
+
+/**
+ * Method 2
+ * @param {number} n
+ * @return {number[]}
+ */
+var grayCode = function (n) {
+    const count = Math.pow(2, n);
+    const res = [], i = 0;
+    for (let i = 0; i < count; i++) {
+        res[i] = i ^ (i >> 1);
+    }
+    return res;
+};

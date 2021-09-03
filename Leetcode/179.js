3e654dc2fb7bf8a9fe8b2b578a106a08c6e42b4c
leo0807 <zhangjunxu3@gmail.com>
2021-09-04 01:06:55 +0800

    179

--- Leetcode/179.js
+++ Leetcode/179.js
@@ -7,3 +7,18 @@ var largestNumber = function (nums) {
     if (nums[0] === 0) return '0';
     return nums.join('');
 };
+var largestNumber2 = function (nums) {
+    const n = nums.length;
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j < n; j++) {
+            if (String(nums[i]) + String(nums[j]) < String(nums[j]) + String(nums[i])) {
+                [nums[i], nums[j]] = [nums[j], nums[i]];
+            }
+        }
+    }
+    let res = "";
+    for (const i of nums) {
+        res += i;
+    }
+    return String(BigInt(res));
+};

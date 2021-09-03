e5548a320398bbbdb3819428f143f592e7207dfe
leo0807 <zhangjunxu3@gmail.com>
2021-09-03 16:27:33 +0800

    73

--- Leetcode/73.js
+++ Leetcode/73.js
@@ -26,3 +26,35 @@ var setZeroes = function (matrix) {
     }
     // return matrix;
 };
+
+
+// [[0, 1, 2, 0],
+//  [3, 4, 5, 2],
+//  [1, 3, 1, 5]]
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var setZeroes = function (matrix) {
+    const rowNum = matrix.length,
+        colNum = matrix[0].length;
+    let rowSign = false, colSign = false;
+    for (let i = 0; i < rowNum; i++) {
+        for (let j = 0; j < colNum; j++) {
+            if (matrix[i][j] === 0) {
+                if (i === 0) rowSign = true;
+                if (j === 0) colSign = true;
+                matrix[i][0] = matrix[0][j] = 0;
+            }
+        }
+    }
+    for (let i = 1; i < rowNum; i++) {
+        for (let j = 1; j < colNum; j++) {
+            if (matrix[i][0] === 0 || matrix[0][j] === 0) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+    if (rowSign) for (let i = 0; i < colNum; i++) matrix[0][i] = 0;
+    if (colSign) for (let i = 0; i < rowNum; i++) matrix[i][0] = 0;
+};

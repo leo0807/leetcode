ed367cf38eb159d6e140e30c8f523e1da5bbafa1
leo0807 <zhangjunxu3@gmail.com>
2021-09-04 20:24:39 +0800

    189

--- Leetcode/189.js
+++ Leetcode/189.js
@@ -9,7 +9,6 @@ var rotate = function (nums, k) {
     reverseArr(nums, 0, n - 1);
     reverseArr(nums, 0, k - 1);
     reverseArr(nums, k, n - 1);
-    return nums;
 };
 function reverseArr(arr, begin, end) {
     while (begin < end) {

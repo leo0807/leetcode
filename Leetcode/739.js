9fa50427c8c67417ff0ca94d454f5f71e3d426e9
leo0807 <zhangjunxu3@gmail.com>
2021-09-05 16:25:19 +0800

    739

--- Leetcode/739.js
+++ Leetcode/739.js
@@ -15,3 +15,19 @@ var dailyTemperatures = function (T) {
     }
     return res;
 };
+
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+var dailyTemperatures2 = function (temperatures) {
+    let res = [], stack = [], n = temperatures.length;
+    for (let i = n - 1; i >= 0; i--) {
+        while (stack.length && temperatures[stack[stack.length - 1]] <= temperatures[i]) {
+            stack.pop();
+        }
+        res[i] = stack.length === 0 ? 0 : (stack[stack.length - 1] - i);
+        stack.push(i);
+    }
+    return res;
+};

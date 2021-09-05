cea5cbe35719ed621540442699f050eb968abb63
leo0807 <zhangjunxu3@gmail.com>
2021-09-05 23:09:06 +0800

    114

--- Leetcode/114.js
+++ Leetcode/114.js
@@ -8,3 +8,29 @@ var flatten = function (root) {
     root.left = null;
     prev = root;
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {void} Do not return anything, modify root in-place instead.
+ */
+var flatten2 = function (root) {
+    if (!root) return null;
+    flatten(root.left);
+    flatten(root.right);
+    let left = root.left, right = root.right;
+    root.left = null;
+    root.right = left;
+    let node = root;
+    while (node.right) {
+        node = node.right;
+    }
+    node.right = right;
+};

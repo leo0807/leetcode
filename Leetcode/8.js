51a0de13952a10fe760b51ce73aa4dcb4c40a24a
leo0807 <zhangjunxu3@gmail.com>
2021-09-07 01:18:53 +0800

    8

--- Leetcode/8.js
+++ Leetcode/8.js
@@ -2,7 +2,32 @@
  * @param {string} s
  * @return {number}
  */
-var myAtoi = function(s) {
+var myAtoi = function (s) {
     let res = s.trim().match(/^(\-|\+)?\d+/g);
-    return res?Math.max(Math.min(Number(res[0]), 2**31-1), -(2**31)):0
+    return res ? Math.max(Math.min(Number(res[0]), 2 ** 31 - 1), -(2 ** 31)) : 0
+};
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var myAtoi = function (s) {
+    let res = 0, index = 0, flag = 1, n = s.length;
+    const max = (Math.pow(2, 31) - 1);
+    const min = Math.pow(-2, 31);
+    while (s[index] === ' ') index++;
+    if (s[index] === '-') {
+        index++;
+        flag = -1;
+    } else if (s[index] === '+') index++;
+    while (index < n && s[index] != ' ' && !isNaN(s[index])) {
+        let digit = Number(s[index]);
+        // console.log(res);
+        if (res > (max / 10) || (res === Math.floor(max / 10) && digit > 7)) {
+            return flag === 1 ? max : min;
+        }
+        res = res * 10 + digit;
+        index++;
+    }
+    return flag * res;
 };

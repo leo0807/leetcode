222130d7c771233bd3842793496f97ff6e07f2ed
leo0807 <zhangjunxu3@gmail.com>
2021-09-06 10:05:43 +0800

    add iterative method

--- Leetcode/98.js
+++ Leetcode/98.js
@@ -28,3 +28,32 @@ function traverse(root, res) {
     res.push(root.val);
     traverse(root.right, res);
 }
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function (root) {
+    if (!root) return true;
+    const stack = [];
+    let inorder = -Infinity;
+    while (stack.length || root) {
+        while (root) {
+            stack.push(root);
+            root = root.left;
+        }
+        root = stack.pop();
+        if (root.val <= inorder) return false;
+        inorder = root.val;
+        root = root.right;
+    }
+    return true;
+};

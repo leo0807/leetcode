5d2ebac5f919798d26d6f53a8285c37e506d3792
leo0807 <zhangjunxu3@gmail.com>
2021-09-07 22:12:36 +0800

    637

---
+++ Leetcode/637.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var averageOfLevels = function (root) {
+    if (!root) return [];
+    const queue = [root], res = [];
+    while (queue.length) {
+        // 当前层数的节点长度
+        const n = queue.length;
+        let tmp = 0;
+        for (let i = 0; i < n; i++) {
+            const node = queue.shift();
+            tmp += node.val;
+            node.left && queue.push(node.left);
+            node.right && queue.push(node.right);
+        }
+        res.push(tmp / n);
+    }
+    return res;
+};

b4f6752b78b1d6ba0701a4f7fb40e07c5c88cf9f
leo0807 <zhangjunxu3@gmail.com>
2021-09-11 19:15:53 +0800

    258

--- Leetcode/258.js
+++ Leetcode/258.js
@@ -1,27 +1,27 @@
 /**
- * @param {string} pattern
- * @param {string} s
- * @return {boolean}
+ * @param {number} num
+ * @return {number}
  */
-var wordPattern = function (pattern, s) {
-    const map = new Map(), n = pattern.length;
-    s = s.split(' ');
-    if (s.length !== pattern.length) return false;
-    for (let i = 0; i < n; i++) {
-        const a = pattern.charAt(i);
-        if (!map.has(a)) {
-            map.set(a, s[i]);
-        } else {
-            if (map.get(a) !== s[i]) {
-                return false;
-            }
+var addDigits = function (num) {
+    if (num > 9) {
+        num %= 9;
+        if (num === 0) return 9;
+    }
+    return num;
+};
+// 假如一个三位数'abc'，其值大小为s1 = 100 * a + 10 * b + 1 * c，经过一次各位相加后，变为s2 = a + b + c，减小的差值为(s1 - s2) = 99 * a + 9 * b，差值可以被9整除，每一个循环都这样，缩小了9的倍数。当num小于9，即只有一位时，直接返回num，大于9时，如果能被9整除，则返回9（因为不可能返回0也不可能返回两位数及以上的值），如果不能被整除，就返回被9除的余数。
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var addDigits = function (num) {
+    while (num > 9) {
+        let tmp = 0;
+        while (num !== 0) {
+            tmp += num % 10;
+            num = Math.floor(num / 10);
         }
+        num = tmp;
     }
-    const valMap = new Map();
-    for (const i of map.values()) {
-        if (!valMap.has(i)) valMap.set(i, i);
-        else return false;
-    }
-
-    return true;
+    return num;
 };

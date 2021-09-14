995fd82fcedd1295664b645bf28f1394fb657317
leo0807 <zhangjunxu3@gmail.com>
2021-09-15 00:54:04 +0800

    524

--- Leetcode/524.js
+++ Leetcode/524.js
@@ -24,3 +24,41 @@ var findLongestWord = function (s, dictionary) {
     // console.log(res);
     return res;
 };
+
+var findLongestWord = function (s, dictionary) {
+    const m = s.length;
+    const f = new Array(m + 1).fill(0).map(() => new Array(26).fill(m));
+
+    for (let i = m - 1; i >= 0; --i) {
+        for (let j = 0; j < 26; ++j) {
+            if (s[i] === String.fromCharCode('a'.charCodeAt() + j)) {
+                f[i][j] = i;
+            } else {
+                f[i][j] = f[i + 1][j];
+            }
+        }
+    }
+    let res = "";
+    for (const t of dictionary) {
+        let match = true;
+        let j = 0;
+        for (let i = 0; i < t.length; ++i) {
+            if (f[j][t[i].charCodeAt() - 'a'.charCodeAt()] === m) {
+                match = false;
+                break;
+            }
+            j = f[j][t[i].charCodeAt() - 'a'.charCodeAt()] + 1;
+        }
+        if (match) {
+            if (t.length > res.length || (t.length === res.length && t.localeCompare(res) < 0)) {
+                res = t;
+            }
+        }
+    }
+    return res;
+};
+
+作者：LeetCode - Solution
+链接：https://leetcode-cn.com/problems/longest-word-in-dictionary-through-deleting/solution/tong-guo-shan-chu-zi-mu-pi-pei-dao-zi-di-at66/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。

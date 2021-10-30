9a562d337ba4b5c8eb2f51801137ef7e80ef1275
leo0807 <zhangjunxu3@gmail.com>
2021-10-30 23:45:50 +0800

    42

--- Leetcode/42.js
+++ Leetcode/42.js
@@ -16,24 +16,112 @@ function trap(height) {
     return res;
 };
 
-function find_subarrays(arr, target) {
-    let result = [],
-        product = 1,
-        left = 0;
-    for (right = 0; right < arr.length; right++) {
-        product *= arr[right];
-        while ((product >= target && left < arr.length)) {
-            product /= arr[left];
-            left += 1;
+// 双指针 按照列计算雨水高度
+var trap = function (height) {
+    let sum = 0;
+    for (let i = 0; i < height.length; i++) {
+        // 第一个柱子和最合一个柱子不接雨水
+        if (i === 0 || i === height.length - 1) continue;
+        // 记录左右两边最高的柱子
+        let rHeight = height[i];
+        let lHeight = height[i];
+        for (let r = i + 1; r < height.length; r++) {
+            if (height[r] > rHeight) rHeight = height[r];
         }
-        // since the product of all numbers from left to right is less than the target therefore,
-        // all subarrays from left to right will have a product less than the target too; to avoid
-        // duplicates, we will start with a subarray containing only arr[right] and then extend it
-        const tempList = [];
-        for (let i = right; i > left - 1; i--) {
-            tempList.unshift(arr[i]);
-            result.push(tempList.toArray());
+        for (let l = i - 1; l >= 0; l--) {
+            if (height[l] > lHeight) lHeight = height[l];
         }
+        const h = Math.min(lHeight, rHeight) - height[i];
+        if (h > 0) sum += h;
     }
-    return result;
+    return sum;
+};
+
+// 动态规划
+// 为了的到两边的最高高度，使用了双指针来遍历，每到一个柱子都向两边遍历一遍，这其实是有重复计算的。
+// 我们把每一个位置的左边最高高度记录在一个数组上（maxLeft），右边最高高度记录在一个数组上（maxRight）。
+// 这样就避免了重复计算，这就用到了动态规划。
+
+var trap2 = function (height) {
+    if (height.length <= 2) return 0;
+    const size = height.length;
+    const maxLeft = Array(size).fill(0);
+    const maxRight = Array(size).fill(0);
+
+    // 记录每个柱子左边柱子的最大高度
+    maxLeft[0] = height[0];
+    for (let i = 1; i < size; i++) {
+        maxLeft[i] = Math.max(height[i], maxLeft[i - 1]);
+    }
+    maxRight[size - 1] = height[size - 1];
+    for (let i = size - 2; i >= 0; i--) {
+        maxRight[i] = Math.max(height[i], maxRight[i + 1]);
+    }
+    // 求和
+    let sum = 0;
+    for (let i = 0; i < size; i++) {
+        const count = Math.min(maxLeft[i], maxRight[i]) - height[i];
+        if (count > 0) sum += count;
+    }
+    return sum;
+}
+// 单调栈
+// 单调栈存储下标，如果栈内有至少两个元素
+var trap3 = function (height) {
+    let ans = 0;
+    const stack = [];
+    const n = height.length;
+    for (let i = 0; i < n; i++) {
+        // 记栈顶元素为 \textit{top}top，\textit{top}top 的下面一个元素是 \textit{left}left，则一定有 \textit{height}[\textit{left}] \ge \textit{height}[\textit{top}]height[left]≥height[top]
+
+        while (stack.length && height[i] > height[stack[stack.length - 1]]) {
+            const top = stack.pop();
+            if (!stack.length) break;
+            const left = stack[stack.length - 1];
+            const currWidth = i - left + 1;
+            const currHeight = Math.min(height[left], height[i]) - height[top];
+            ans += currHeight * currHeight;
+        }
+        // 为了得到 \textit{left}left，需要将 \textit{top}top 出栈。在对 \textit{top}top 计算能接的雨水量之后，\textit{left}left 变成新的 \textit{top}top
+
+        stack.push(i);
+    }
+    return ans;
+}
+
+// 单调栈2
+// 1. 按照行的方向计算
+// 2. 使用单调栈内元素的顺序
+// 新元素高度大于栈头元素，则说明出现凹槽
+// 3. 遇到相同高度的柱子
+// 更新栈内元素下标，完成置换
+var trap3 = function (height) {
+    const size = height.length;
+    if (size <= 2) return 0;
+    const stack = [];
+    stack.push(0);
+    let sum = 0;
+    for (let i = 1; i < size; i++) {
+        // 当前遍历的元素（柱子）高度小于栈顶元素的高度，就把这个元素加入栈中，因为栈里本来就要保持从小到大的顺序（从栈头到栈底）
+        if (height[i] < height[stack[stack.length - 1]]) {
+            stack.push(i);
+        } else if (height[i] === height[stack[stack.length - 1]]) {
+            // 高度相同
+            stack.pop();
+            stack.push(i);
+        } else {
+            while (!stack.length && height[i] > height[stack[stack.length - 1]]) {
+                const mid = stack[stack.length - 1];
+                stack.pop();
+                if (!stack.length) {
+                    const h = Math.min(height[stack[stack.length - 1]], height[i]) - height[mid];
+                    const w = i - stack[stack.length - 1] - 1;
+                    sum += h * w;
+                }
+            }
+            stack.push(i);
+        }
+
+    }
+    return sum;
 }

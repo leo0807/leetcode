274a8212471f6203ec99eed5e312f126bc8cac6c
leo0807 <zhangjunxu3@gmail.com>
2021-11-01 09:11:15 +0800

    92

--- LintCode/92.js
+++ LintCode/92.js
@@ -11,6 +11,7 @@ export class Solution {
         // write your code here
         const n = A.length;
         const dp = Array(m + 1).fill(0);
+        // dp简化为1维数组，也可以为2维数组
         for (let i = 0; i < n; i++) {
             for (let j = m; j >= A[i]; j--) {
                 dp[j] = Math.max(dp[j], dp[j - A[i]] + A[i]);

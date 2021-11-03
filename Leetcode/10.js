411b37399c3d621caf4d20844cbfe31e49063a0a
leo0807 <zhangjunxu3@gmail.com>
2021-11-03 14:03:40 +0800

    10

--- Leetcode/10.js
+++ Leetcode/10.js
@@ -1,6 +1,8 @@
 var match = function (s, p) {
     if (p.length === 0) return s.length === 0;
+    // 判断第一个字符相匹配的情况
     let first_match = (s.length !== 0) && (s[0] === p[0] || p[0] === '.');
+
     if (p.length >= 2 && p[1] === '*') {
         return match(s, p.substr(2)) || (first_match && match(s.substr(1), p));
     } else {

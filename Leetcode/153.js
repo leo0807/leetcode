5290443ef8a8a32cc8d53448305c1af3ed3ddb42
leo0807 <zhangjunxu3@gmail.com>
2021-11-03 14:04:07 +0800

    153

--- Leetcode/153.js
+++ Leetcode/153.js
@@ -4,13 +4,16 @@
  */
 var findMin = function (nums) {
     let left = 0, right = nums.length - 1;
+    if (nums[left] < nums[right]) return nums[left];
     while (left < right) {
-        let mid = (left + right) >> 1;
-        if (nums[mid] < nums[right]) {
-            right = mid;
-        } else {
-            left = mid + 1;
+        const mid = left + Math.floor((right - left) / 2);
+        if (nums[mid + 1] < nums[mid]) {
+            return nums[mid + 1];
+        } else if (nums[mid] < nums[mid - 1]) {
+            return nums[mid];
         }
+        if (nums[0] < nums[mid]) left = mid + 1;
+        else right = mid
     }
     return nums[left];
 };

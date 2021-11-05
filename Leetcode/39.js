9ae76dca83c1e9b3e26bd6901b8af3db10a68923
leo0807 <zhangjunxu3@gmail.com>
2021-11-05 23:01:36 +0800

    39

--- Leetcode/39.js
+++ Leetcode/39.js
@@ -40,3 +40,32 @@ var combinationSum2 = function (candidates, target) {
     dfs(0, target, []);
     return res;
 };
+
+
+
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum3 = function (candidates, target) {
+    const path = [], res = [], n = candidates.length;
+    const dfs = function (sum, currentIndex) {
+        if (sum > target) return;
+        if (sum === target) {
+            res.push(Array.from(path));
+            return;
+        }
+        for (let i = currentIndex; i < n; i++) {
+            // 添加当前元素
+            sum += candidates[i];
+            // 添加到后备路径，用以后续使用
+            path.push(candidates[i]);
+            // 不增加i，因为元素可以无限选取
+            dfs(sum, i);
+            sum -= path.pop();
+        }
+    }
+    dfs(0, 0);
+    return res;
+};

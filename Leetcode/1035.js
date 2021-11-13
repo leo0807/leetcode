0b32adbd0a49265b47622ff356dd5322f41fbe3a
leo0807 <zhangjunxu3@gmail.com>
2021-11-14 00:27:27 +0800

    1035

--- Leetcode/1035.js
+++ Leetcode/1035.js
@@ -16,3 +16,25 @@ var maxUncrossedLines = function (A, B) {
     console.log(dp);
     return dp[m][n];
 };
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ * 此题目等同于1143，不相交的线
+ * 本题说是求绘制的最大连线数，其实就是求两个字符串的最长公共子序列的长度！
+ */
+var maxUncrossedLines = function (nums1, nums2) {
+    const n = nums1.length, m = nums2.length;
+    const dp = [...Array(n + 1)].map(() => new Array(m + 1).fill(0));
+    for (let i = 1; i <= n; i++) {
+        for (let j = 1; j <= m; j++) {
+            if (nums1[i - 1] === nums2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                dp[i][j] = Math.max.call(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+    return dp[n][m];
+};

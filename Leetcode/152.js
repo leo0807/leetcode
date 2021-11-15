3d107ce1b741ece68de69704b30d7715a3b64291
leo0807 <zhangjunxu3@gmail.com>
2021-11-15 09:18:18 +0800

    152

--- Leetcode/152.js
+++ Leetcode/152.js
@@ -17,3 +17,22 @@ var maxProduct = function (nums) {
     }
     return res;
 };
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function (nums) {
+    const n = nums.length;
+    const dp = [...Array(n)].map(() => new Array(2).fill(0));
+    dp[0][0] = nums[0];
+    dp[0][1] = nums[0];
+    let res = nums[0];
+    for (let i = 1; i < n; i++) {
+        dp[i][0] = Math.max.call(null, dp[i - 1][0] * nums[i], nums[i], dp[i - 1][1] * nums[i]);
+        dp[i][1] = Math.min.call(null, dp[i - 1][0] * nums[i], nums[i], dp[i - 1][1] * nums[i]);
+        res = Math.max(res, dp[i][0]);
+    }
+    // console.log(dp);
+    return res;
+};

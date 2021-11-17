3339bd14bab1a5cee4d03afd802a2a55fab2704d
leo0807 <zhangjunxu3@gmail.com>
2021-11-17 19:38:09 +0800

    122

--- Leetcode/122.js
+++ Leetcode/122.js
@@ -20,13 +20,13 @@ var maxProfit2 = function (prices) {
   const n = prices.length;
   const dp = new Array(n, () => new Array(2).fill(0));
   /**
-   * dp[i][0] 不持有股票的利润
+   * dp[i][1] 不持有股票的利润
    * dp[i][0] 持有股票的利润
    */
-  dp[0][0] = -prices[0][0]; // 定义初始值为 
+  dp[0][0] = -prices[0]; // 定义初始值为 
   for (let i = 1; i < n; i++) {
     // 第i天持股票所剩最多现金 = max(第i-1天持股票所剩现金, 第i-1天持现金-买第i天的股票)
-    dp[i][0] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+    dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] - prices[i]);
     // 第i天持有最多现金 = max(第i-1天持有的最多现金，第i-1天持有股票的最多现金+第i天卖出股票)
     dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
   }

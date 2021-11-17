47eb0f559c6bcb491291110a1c03ee4a1b7842a6
leo0807 <zhangjunxu3@gmail.com>
2021-11-17 11:21:37 +0800

    662

--- Leetcode/662.js
+++ Leetcode/662.js
@@ -11,50 +11,19 @@
  * @return {number}
  */
 var widthOfBinaryTree = function (root) {
-    if (!root) return 0;
-    let queue = [[root, 0n]];
     let max = 0n;
+    // if(!root) return 0;
+    const queue = [[root, 0n]];
     while (queue.length) {
         const n = queue.length;
-        let left = queue[0][1], right = 0, tmp = [];
-
+        let maxVal = queue[n - 1][1], minVal = queue[0][1];
+        max = max > maxVal - minVal + 1n ? max : maxVal - minVal + 1n;
         for (let i = 0; i < n; i++) {
-            const [node, pos] = queue[i];
+            const [node, val] = queue.shift();
 
-            if (node.left) {
-                right = pos * 2n;
-                tmp.push([node.left, right]);
-            }
-            if (node.right) {
-                right = pos * 2n + 1n;
-                tmp.push([node.right, right]);
-            }
-            right = pos;
+            node.left && queue.push([node.left, 2n * val]);
+            node.right && queue.push([node.right, 2n * val + 1n]);
         }
-
-        queue = tmp;
-        max = max > right - left + 1n ? max : right - left + 1n;
     }
     return max;
-};
-
-
-var widthOfBinaryTree2 = function (root) {
-    if (!root) {
-        return 0
-    }
-    let ans = 1, que = [[0, root]]
-    while (que.length) {
-        ans = Math.max(ans, que[que.length - 1][0] - que[0][0] + 1)
-        let tmp = []
-        for (const [i, q] of que) {
-            q.left && tmp.push([i * 2, q.left])
-            q.right && tmp.push([i * 2 + 1, q.right])
-        }
-        if (que[0][0] > 1 << 31) {
-            tmp.forEach(v => v[0] -= que[0][0])
-        }
-        que = tmp
-    }
-    return ans
 };

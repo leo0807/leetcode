cb91479f792ea0f39a764db9dcaaba07efdca555
leo0807 <zhangjunxu3@gmail.com>
2021-11-20 23:26:24 +0800

    173

--- Leetcode/173.js
+++ Leetcode/173.js
@@ -31,7 +31,7 @@ BSTIterator.prototype.next = function () {
         this.stack.push(node);
         node = node.left;
     }
-    return res;;
+    return res;
 };
 
 /**
@@ -39,6 +39,53 @@ BSTIterator.prototype.next = function () {
  */
 BSTIterator.prototype.hasNext = function () {
     return this.stack.length !== 0;
+};
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * var obj = new BSTIterator(root)
+ * var param_1 = obj.next()
+ * var param_2 = obj.hasNext()
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ */
+var BSTIterator = function (root) {
+    const stack = [];
+    this.queue = [];
+    while (stack.length || root) {
+        if (root) {
+            stack.push(root);
+            root = root.left;
+        } else {
+            root = stack.pop();
+            this.queue.push(root.val);
+            root = root.right;
+        }
+    }
+};
+
+/**
+ * @return {number}
+ */
+BSTIterator.prototype.next = function () {
+    return this.queue.shift();
+};
+
+/**
+ * @return {boolean}
+ */
+BSTIterator.prototype.hasNext = function () {
+    // console.log(this.queue);
+    return this.queue.length !== 0;
 };
 
 /**

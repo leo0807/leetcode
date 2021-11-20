880dd8ddd8e75d0f873703f633527930217e0184
leo0807 <zhangjunxu3@gmail.com>
2021-11-20 11:24:11 +0800

    116

--- Leetcode/116.js
+++ Leetcode/116.js
@@ -22,3 +22,23 @@ var connect = function (root) {
     root.right = connect(root.right);
     return root;
 };
+
+var connect = function (root) {
+    if (!root) return root;
+    const queue = [root];
+    while (queue.length) {
+        const n = queue.length;
+        const tmp = [];
+        for (let i = 0; i < n; i++) {
+            const node = queue.shift();
+            tmp.push(node);
+            node.left && queue.push(node.left);
+            node.right && queue.push(node.right);
+        }
+        for (let i = 1; i < tmp.length; i++) {
+            tmp[i - 1].next = tmp[i];
+        }
+        tmp[tmp.length - 1].next = null;
+    }
+    return root;
+};

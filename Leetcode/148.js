21024cd23a11e300790a3932070d22adc2c9ef51
leo0807 <zhangjunxu3@gmail.com>
2021-11-22 09:22:12 +0800

    148

--- Leetcode/148.js
+++ Leetcode/148.js
@@ -42,3 +42,48 @@ var sortList = function (head) {
     lhead.next = left === null ? right : left;
     return res.next;
 };
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var sortList = function (head) {
+    return mergeSort(head);
+};
+
+function mergeSort(head) {
+    if (!head || !head.next) return head;
+    let slow = head, fast = head.next;
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    let right = sortList(slow.next);
+    slow.next = null;
+    let left = sortList(head);
+    return merge(left, right);
+}
+function merge(left, right) {
+    let h = new ListNode();
+    let p = h;
+    while (left && right) {
+        if (left.val < right.val) {
+            p.next = left;
+            left = left.next;
+        } else {
+            p.next = right;
+            right = right.next;
+        }
+        p = p.next;
+    }
+    p.next = left === null ? right : left;
+    return h.next;
+}

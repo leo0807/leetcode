bbf1ffbf98ff2768c6a7178c54af979e3403f13d
leo0807 <zhangjunxu3@gmail.com>
2021-11-29 15:59:45 +0800

    378

--- Leetcode/378.py
+++ Leetcode/378.py
@@ -7,3 +7,29 @@ class Solution:
         for i in range(k - 1):
             heapq.heappop(minHeap)
         return heapq.heappop(minHeap)
+
+
+class Solution:
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        n = len(matrix)
+
+        def check(mid):
+            i, j = n - 1, 0
+            num = 0
+            while i >= 0 and j < n:
+                if matrix[i][j] <= mid:
+                    num += i + 1
+                    j += 1
+                else:
+                    i -= 1
+            return num >= k
+
+        left, right = matrix[0][0], matrix[-1][-1]
+        while left <= right:
+            mid = (left + right) // 2
+            if check(mid):
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return left

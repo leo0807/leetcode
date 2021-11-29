3807bad636459f25179d73b748715dc914f1a2af
leo0807 <zhangjunxu3@gmail.com>
2021-11-29 09:24:25 +0800

    JZ53

---
+++ 剑指offer/JZ53.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function (nums) {
+  const n = nums.length;
+  let index = 0;
+  for (let i = 0; i < index; i++) {
+    if (i !== nums[i]) {
+      [nums[nums[i]], nums[i]] =
+        [nums[i], nums[nums[i]]];
+    }
+  }
+
+  for (let i = 0; i < n; i++) {
+    if (i !== nums[i]) return i;
+  }
+  return n > 0 ? n : 0;
+};

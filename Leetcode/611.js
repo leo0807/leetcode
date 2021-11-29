21313065437355e8ae7b736f5bee64cf5db4ca23
leo0807 <zhangjunxu3@gmail.com>
2021-11-29 08:56:26 +0800

    611

--- Leetcode/611.js
+++ Leetcode/611.js
@@ -18,3 +18,30 @@ var triangleNumber = function (nums) {
     }
     return res;
 };
+
+var triangleNumber = function (nums) {
+    const n = nums.length;
+    nums.sort((a, b) => a - b);
+    let ans = 0;
+    for (let i = 0; i < n; ++i) {
+        for (let j = i + 1; j < n; ++j) {
+            let left = j + 1, right = n - 1, k = j;
+            while (left <= right) {
+                const mid = Math.floor((left + right) / 2);
+                if (nums[mid] < nums[i] + nums[j]) {
+                    k = mid;
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+            ans += k - j;
+        }
+    }
+    return ans;
+};
+
+// 作者：LeetCode - Solution
+// 链接：https://leetcode-cn.com/problems/valid-triangle-number/solution/you-xiao-san-jiao-xing-de-ge-shu-by-leet-t2td/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。

2afaddd7870b7364f0a6f8763fad38adcbc90488
leo0807 <zhangjunxu3@gmail.com>
2021-12-04 23:59:58 +0800

    98

--- Leetcode/98.js
+++ Leetcode/98.js
@@ -58,20 +58,6 @@ var isValidBST = function (root) {
     return true;
 };
 
-
-// Recursive
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
- * @param {TreeNode} root
- * @return {boolean}
- */
 var isValidBST = function (root) {
     return validation(root, -Infinity, Infinity);
 };

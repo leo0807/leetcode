8f69a2762e076df6f2c6c6642bfdfc67402dff61
leo0807 <zhangjunxu3@gmail.com>
2021-12-11 18:53:45 +0800

    update

--- 剑指offer/JZ53.js
+++ 剑指offer/JZ53.js
@@ -5,7 +5,7 @@
 var missingNumber = function (nums) {
   const n = nums.length;
   let index = 0;
-  for (let i = 0; i < index; i++) {
+  for (let i = 0; i < n; i++) {
     if (i !== nums[i]) {
       [nums[nums[i]], nums[i]] =
         [nums[i], nums[nums[i]]];

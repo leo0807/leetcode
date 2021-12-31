720ca1ac9d99d3a23028f8361e8d9316945c67ea
leo0807 <zhangjunxu3@gmail.com>
2021-12-31 23:26:03 +0800

    add comment

--- Leetcode/PrefixSum/560.js
+++ Leetcode/PrefixSum/560.js
@@ -3,11 +3,13 @@
  * @param {number} k
  * @return {number}
  */
- 
+
 var subarraySum = function (nums, k) {
   let count = 0, map = new Map(), pre = 0;
+  // 初始化
   map.set(0, 1)
   for (const i of nums) {
+
     pre += i;
     if (map.has(pre - k)) {
       count += map.get(pre - k);

e805b13d534a026e344f0df4b3c21b65c077dd54
leo0807 <zhangjunxu3@gmail.com>
2023-11-09 10:20:34 +0800

    daily update

--- Leetcode/290.js
+++ Leetcode/290.js
@@ -28,3 +28,26 @@ var wordPattern = function (pattern, s) {
 
     return true;
 };
+
+/**
+ * @param {string} pattern
+ * @param {string} s
+ * @return {boolean}
+ */
+var wordPattern = function (pattern, s) {
+    const word2ch = new Map();
+    const ch2word = new Map();
+    const words = s.split(' ');
+    if (pattern.length !== words.length) {
+        return false;
+    }
+    for (const [i, word] of words.entries()) {
+        const ch = pattern[i];
+        if (word2ch.has(word) && word2ch.get(word) != ch || ch2word.has(ch) && ch2word.get(ch) !== word) {
+            return false;
+        }
+        word2ch.set(word, ch);
+        ch2word.set(ch, word);
+    }
+    return true;
+};

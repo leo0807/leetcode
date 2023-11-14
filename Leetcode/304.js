8a895ce4a65fefb0a201f83488ce55dd44e80508
leo0807 <zhangjunxu3@gmail.com>
2023-11-14 21:45:54 +0800

    304

--- Leetcode/304.js
+++ Leetcode/304.js
@@ -36,3 +36,42 @@ NumMatrix.prototype.sumRegion = function (row1, col1, row2, col2) {
  * var obj = new NumMatrix(matrix)
  * var param_1 = obj.sumRegion(row1,col1,row2,col2)
  */
+
+
+
+/**
+ * @param {number[][]} matrix
+ * Two Dismension Solution
+ */
+var NumMatrix = function (matrix) {
+  const row = matrix.length;
+  if (row > 0) {
+    const col = matrix[0].length;
+    this.sums = new Array(row + 1).fill(0).map(() => new Array(col + 1).fill(0));
+    for (let i = 0; i < row; ++i) {
+      for (let j = 0; j < col; ++j) {
+        this.sums[i + 1][j + 1] = this.sums[i][j + 1] + this.sums[i + 1][j]
+          - this.sums[i][j]
+          + matrix[i][j];
+      }
+    }
+  }
+
+};
+
+/** 
+ * @param {number} row1 
+ * @param {number} col1 
+ * @param {number} row2 
+ * @param {number} col2
+ * @return {number}
+ */
+NumMatrix.prototype.sumRegion = function (row1, col1, row2, col2) {
+  return this.sums[row2 + 1][col2 + 1] - this.sums[row1][col2 + 1] - this.sums[row2 + 1][col1] + this.sums[row1][col1];
+};
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * var obj = new NumMatrix(matrix)
+ * var param_1 = obj.sumRegion(row1,col1,row2,col2)
+ */

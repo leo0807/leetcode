4e8eb0b9529f40ec01d2f30e3a31dad98e0a430a
leo0807 <zhangjunxu3@gmail.com>
2023-11-14 19:32:30 +0800

    Updated files: Leetcode/304.js auto.sh

--- auto.sh
+++ auto.sh
@@ -3,27 +3,30 @@ echo "================="
 echo "auto git by Junxu"
 echo "======= 🤪  ====="
 
-echo -e  "
-▶ \033[33;1mgit add .
-\033[0m"
+echo -e "\n▶ \033[33;1mgit add .\033[0m"
 git add .
 
 git status
-echo -e "
-▶ \033[33;1mcommit message:
-\033[37;1m" 
-# read msg
 
-echo -e "
-▶ \033[33;1mgit commit -m daily-update
-\033[0m"
-git commit -m "daily update"
+# Prompt for a commit message
+echo -e "\n▶ \033[33;1mEnter commit message (Press Enter to auto-generate):\033[37;1m"
+read -r msg
 
-echo -e "
-▶ \033[33;1mgit push
-"
-echo -e "\033[37;1mstart pushing ...\033[0m
-"
+# Use the entered commit message or generate one if none entered
+if [[ -z "$msg" ]]; then
+    # Get list of staged files
+    files=$(git diff --cached --name-only)
+    if [[ -z "$files" ]]; then
+        echo "No files to commit."
+        exit 1
+    fi
+    msg="Updated files: $files"
+fi
+
+echo -e "\n▶ \033[33;1mCommitting changes...\033[0m"
+git commit -m "$msg"
+
+echo -e "\n▶ \033[33;1mgit push\033[0m"
+echo -e "\033[37;1mstart pushing ...\033[0m\n"
 git push origin master
-echo -e "
-\033[37;1mAll Done\033[0m"
+echo -e "\n\033[37;1mAll Done\033[0m"

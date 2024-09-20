9f9e82fd2070a9047e195b0436e319a842b08d5a
Scott <zhangjunxu3@gmail.com>
2024-09-20 20:01:53 +0800

    Update 222.js

--- Leetcode/222.js
+++ Leetcode/222.js
@@ -22,3 +22,31 @@ var countNodes = function (root) {
   }
   return res;
 };
+
+
+
+// Recursion
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var countNodes = function(root) {
+    let res = 0;
+    var count = function (node) {
+        if (!node) return;
+        res++;
+        count(node.left);
+        count(node.right);
+    }
+    count(root);
+    return res;
+};
+
